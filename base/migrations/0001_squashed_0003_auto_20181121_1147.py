176979b5d02511a83013e07fa9b37f916d612bd5
Daniel Zavala-Svensson <raket_83@yahoo.se>
2018-11-28 15:53:56 +0100

    Fixed linter warnings that should not be obeyed.
    Mypy was giving annoying errors that some empty lists in migrations were
    not type hinted. They probably should not be typed since that could possibly
    brake migrations. So I added some mypy ignore annotation.

--- base/migrations/0001_squashed_0003_auto_20181121_1147.py
+++ base/migrations/0001_squashed_0003_auto_20181121_1147.py
@@ -6,46 +6,66 @@ import django.db.models.deletion
 
 class Migration(migrations.Migration):
 
-    replaces = [('base', '0001_initial'), ('base', '0002_user_foo'), ('base', '0003_auto_20181121_1147')]
+    replaces = [
+        ("base", "0001_initial"),
+        ("base", "0002_user_foo"),
+        ("base", "0003_auto_20181121_1147"),
+    ]
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []  # type: ignore
 
     operations = [
         migrations.CreateModel(
-            name='Message',
+            name="Message",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('created', models.DateTimeField(auto_now_add=True)),
-                ('text', models.TextField()),
-                ('updated', models.DateTimeField(auto_now=True)),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("created", models.DateTimeField(auto_now_add=True)),
+                ("text", models.TextField()),
+                ("updated", models.DateTimeField(auto_now=True)),
             ],
-            options={
-                'db_table': 'message',
-                'managed': True,
-            },
+            options={"db_table": "message", "managed": True},
         ),
         migrations.CreateModel(
-            name='User',
+            name="User",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=32, unique=True)),
-                ('created', models.DateTimeField(auto_now_add=True)),
-                ('deactivated', models.DateTimeField(blank=True, null=True)),
-                ('active', models.BooleanField()),
-                ('public_key', models.CharField(blank=True, max_length=178, null=True)),
-                ('email', models.CharField(blank=True, max_length=40, null=True)),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("name", models.CharField(max_length=32, unique=True)),
+                ("created", models.DateTimeField(auto_now_add=True)),
+                ("deactivated", models.DateTimeField(blank=True, null=True)),
+                ("active", models.BooleanField()),
+                (
+                    "public_key",
+                    models.CharField(blank=True, max_length=178, null=True),
+                ),
+                ("email", models.CharField(blank=True, max_length=40, null=True)),
             ],
-            options={
-                'db_table': 'user',
-                'managed': True,
-            },
+            options={"db_table": "user", "managed": True},
         ),
         migrations.AddField(
-            model_name='message',
-            name='createdby',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.DO_NOTHING, related_name='username', to='base.User'),
+            model_name="message",
+            name="createdby",
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.DO_NOTHING,
+                related_name="username",
+                to="base.User",
+            ),
         ),
     ]
