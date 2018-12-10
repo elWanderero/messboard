0d9cf0dfea36499092c5958305cea433fcc1ec17
Daniel Zavala-Svensson <raket_83@yahoo.se>
2018-12-10 18:14:21 +0100

    Made first cursory, working, views.
    With login required.

--- sajt/views.py
+++ sajt/views.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+
+from base.models import Message
 from django.shortcuts import render
 
-# Create your views here.
+
+@login_required
+def index(request) -> HttpResponse:
+    messages = Message.objects.filter(author=request.user.id).only("text")
+    context = {"messages": messages}
+    return render(request, "sajt/index.html", context)
+
+
+@login_required
+def my_messages(request) -> HttpResponse:
+    return HttpResponse("Here you can very clearly see your own messages.")
