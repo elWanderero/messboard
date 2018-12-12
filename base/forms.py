8f918f7931ecb62f13fc4f292695b0ca0bc5d57b
Daniel Zavala-Svensson <raket_83@yahoo.se>
2018-12-12 18:33:23 +0100

    Tidied up
    The fields are overridden anyways somewhere else I think, but must be
    declared. So I set them to the parents default, that should be fine if
    they stop being overridden somehow.

--- base/forms.py
+++ base/forms.py
@@ -1,25 +1,19 @@
-import logging
-
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserChangeForm as AuthChangeForm, \
     UserCreationForm as AuthCreationForm
 
-logger = logging.getLogger(__name__)
-
 
 class UserCreationForm(AuthCreationForm):
     class Meta(AuthCreationForm):
         model = get_user_model()
-        fields = ('username', 'email')
+        fields = AuthCreationForm.Meta.fields
 
 
 class UserChangeForm(AuthChangeForm):
     class Meta(AuthChangeForm):
         model = get_user_model()
-        # fields = UserChangeForm.Meta.fields
-        fields = ("subscriptions",)
-        # fields = UserChangeForm.Meta.fields + "subscriptions")  # ('username', 'email')
+        fields = AuthChangeForm.Meta.fields
 
 
 class UserChangeListForm(forms.ModelForm):
