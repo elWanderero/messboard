8967c9e7e4dc4578005bd2e55974bbd8c488ec31
Daniel Zavala-Svensson <raket_83@yahoo.se>
2018-12-13 16:39:04 +0100

    Set root URL to redirect to sajt/index.html

--- messboard/urls.py
+++ messboard/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from sajt.views import index
+
 urlpatterns = [
+    path("", index, name="index"),
     path("sajt/", include("sajt.urls")),
     path("api/", include("api.urls")),
     path("admin/", admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]  # Base URLs
