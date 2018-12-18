c56af94c23f76befd9d8f63ee00ce1b1915b15e4
Daniel Zavala-Svensson <raket_83@yahoo.se>
2018-12-18 15:06:52 +0100

    Implemented view class for user's messages + posting new message
    Tears, blood and sweat was involved. This does not show in code, but
    the elegance had a cost.

--- sajt/urls.py
+++ sajt/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
+from sajt.views import MyMessages
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("myMessages", views.my_messages, name="myMessages")
+    path("myMessages", MyMessages.as_view(), name="myMessages")
 ]
--- sajt/views.py
+++ sajt/views.py
@@ -1,18 +1,22 @@
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse, HttpResponseForbidden
 from django.shortcuts import render
+from django.views.decorators.http import require_safe
+from django.views.generic.edit import CreateView
 
 from base.models import Message
 
 
+@require_safe
 @login_required
 def index(request) -> HttpResponse:
     subs = request.user.subscriptions.all().only("id", "username")
-    messages = Message.objects.filter(author__in=subs).only("text", "created")
+    messages = Message.objects.filter(author__in=subs).only("text", "date_created")
     messages_by_authors = [
         {
             "author": sub.username,
-            "messages": messages.filter(author=sub).values("text", "created"),
+            "messages": messages.filter(author=sub).values("text", "date_created"),
         }
         for sub in subs
     ]
