f5a86a09e4f284380f2a1e05a29700fb2a237a94
Daniel Zavala-Svensson <raket_83@yahoo.se>
2018-12-19 18:02:33 +0100

    Added Message detail managing part of site.

--- sajt/urls.py
+++ sajt/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from sajt.views import MyMessages
 from . import views
+
+app_name = "sajt"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("myMessages", MyMessages.as_view(), name="myMessages")
+    path("message-list", views.MessageList.as_view(), name="message-list"),
+    path("messages/<int:pk>", views.MessageDetail.as_view(), name="message-detail"),
+    path("messages/<int:pk>/delete", views.MessageDelete.as_view(), name="message-delete"),
+    path("messages/<int:pk>/edit", views.MessageEdit.as_view(), name="message-edit")
 ]
--- sajt/views.py
+++ sajt/views.py
@@ -2,10 +2,19 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse, HttpResponseForbidden
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.decorators.http import require_safe
-from django.views.generic.edit import CreateView
+from django.views.generic import DetailView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 from base.models import Message
+
+
+def _user_is_message_owner(view_instance):
+    user = view_instance.request.user
+    author = view_instance.get_object().author
+    return user.is_authenticated and author == user
+
 
 
 @require_safe
@@ -24,12 +33,12 @@ def index(request) -> HttpResponse:
     return render(request, "sajt/index.html", context)
 
 
-class MyMessages(LoginRequiredMixin, CreateView):
+class MessageList(LoginRequiredMixin, CreateView):
     template_name = "sajt/message_list.html"
     # form_class = MessageCreationForm
     fields = ["text"]
     model = Message
-    success_url = "myMessages"
+    success_url = reverse_lazy("sajt:message-list")
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -44,6 +53,57 @@ class MyMessages(LoginRequiredMixin, CreateView):
             return HttpResponseForbidden()
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+class MessageDetail(DetailView):
+    template_name = "sajt/message_detail.html"
+    model = Message
+
+
+class MessageDelete(LoginRequiredMixin, DeleteView):
+    template_name = "sajt/message_delete.html"
+    model = Message
+    success_url = reverse_lazy("sajt:message-list")
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context["short_description"] = self.get_object().__str__()
+    #     print(self.get_object().__str__())
+    #     return context
+
+    def delete(self, request, *args, **kwargs):
+        if _user_is_message_owner(self):
+            return super().delete(request, *args, **kwargs)
+        else:
+            return self.handle_no_permission()
+
+    def dispatch(self, request, *args, **kwargs):
+        if _user_is_message_owner(self):
+            return super().dispatch(request, *args, **kwargs)
+        else:
+            return self.handle_no_permission()
+
+
+class MessageEdit(LoginRequiredMixin, UpdateView):
+    template_name = "sajt/message_edit.html"
+    model = Message
+    fields = ["text"]
+    # success_url = reverse_lazy("sajt:message-list", kwargs={"id": })
+
+    def get_success_url(self):
+        return reverse_lazy("sajt:message-detail", kwargs={"pk": self.object.id})
+
+    def post(self, request, *args, **kwargs):
+        if _user_is_message_owner(self):
+            return super().post(request, *args, **kwargs)
+        else:
+            return self.handle_no_permission()
+
+    def dispatch(self, request, *args, **kwargs):
+        if _user_is_message_owner(self):
+            return super().dispatch(request, *args, **kwargs)
+        else:
+            return self.handle_no_permission()
 
 
 #####################################################################
